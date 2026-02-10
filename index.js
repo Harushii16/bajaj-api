9530ae86bc731556209d9db81ce33b943be84a71
HarushiSingla <harushisingla@gmail.com>
2026-02-10 12:01:02 +0530

    Fix module export for Vercel

--- index.js
+++ index.js
@@ -118,7 +118,19 @@ app.post("/bfhl", (req, res) => {
 });
 
 /* ---------- SERVER ---------- */
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+
+
+module.exports = app;
